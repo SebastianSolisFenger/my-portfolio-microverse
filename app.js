f2da43895ae571a14bf7707655206a077f7ce1d8
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-04 13:07:08 -0300

    Add app.js dynamics

---
+++ app.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector(".nav-hamburger");
+const navlinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navlinks.classList.toggle("activin");
+  menuBtn.classList.toggle("active");
+});
+
+document.querySelectorAll(".links-mob").forEach((link) =>
+  link.addEventListener("click", () => {
+    navlinks.classList.remove("activin");
+    menuBtn.classList.remove("active");
+  })
+);
