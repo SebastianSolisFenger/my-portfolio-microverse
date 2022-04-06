1e90f1b1a2d752a1d85aa077b07e53d773f017f7
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-06 17:54:55 -0300

    finish fixing linters from FORM.JS

--- Form.js
+++ Form.js
@@ -6,37 +6,31 @@ const form = document.querySelector('#form');
 const emailInput = document.querySelector('#email');
 const errorMsgContainer = document.querySelector('#errorMsg');
 const errorMsg = '* Hi there! Please, write the content of the email field in lower case, thanks.*';
-const regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/;
-
+const regexPattern = /^[a-z0-9.!#$%&'+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)$/;
 // *************************************************************************************************
 // <--------------------------FUNCTIONS-------------------------->
 // *************************************************************************************************
-
 function isValidEmail(emailValue) {
-  return regex.test(emailValue);
+  return regexPattern.test(emailValue);
 }
 
-function showError() {
+function displayError() {
   errorMsgContainer.classList.add('showError');
   errorMsgContainer.textContent = errorMsg;
 }
 
-function deleteErrorMessage() {
+function deleteErrorMsg() {
   errorMsgContainer.classList.remove('showError');
   errorMsgContainer.innerHTML = '';
 }
-
-// *************************************************************************************************
-// <--------------------------SUBMIT-------------------------->
-// *************************************************************************************************
 
 form.addEventListener('submit', (event) => {
   const emailValue = emailInput.value;
 
   if (!isValidEmail(emailValue)) {
     event.preventDefault();
-    showError();
+    displayError();
   } else {
-    deleteErrorMessage();
+    deleteErrorMsg();
   }
 });
