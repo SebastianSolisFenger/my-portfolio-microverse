74529ecc48a5de26e5fa7ae69a32f5dca945084b
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-06 17:18:49 -0300

    Fix function validation and style

--- Form.js
+++ Form.js
@@ -1,21 +1,42 @@
 // *************************************************************************************************
-// <--------------------------FORM-VALIDATION-------------------------->
+// <--------------------------FORM-SECTION-VAR-------------------------->
 // *************************************************************************************************
 
-const form = document.querySelector("#form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const msgInput = document.querySelector("#msg");
-const errorMsgContainer = document.querySelector("#errorMsg");
-const errorMsg = "* The content of the email field has to be in lower case.";
-const regex =
-  /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/;
+const form = document.querySelector('#form');
+const emailInput = document.querySelector('#email');
+const errorMsgContainer = document.querySelector('#errorMsg');
+const errorMsg = '* Hi there! Please, write the content of the email field in lower case, thanks.*';
+const regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/;
+
+// *************************************************************************************************
+// <--------------------------FUNCTIONS-------------------------->
+// *************************************************************************************************
 
 function isValidEmail(emailValue) {
   return regex.test(emailValue);
 }
 
 function showError() {
-  errorMsgContainer.classList.add("showError");
+  errorMsgContainer.classList.add('showError');
   errorMsgContainer.textContent = errorMsg;
 }
+
+function deleteErrorMessage() {
+  errorMsgContainer.classList.remove('showError');
+  errorMsgContainer.innerHTML = '';
+}
+
+// *************************************************************************************************
+// <--------------------------SUBMIT-------------------------->
+// *************************************************************************************************
+
+form.addEventListener('submit', (event) => {
+  const emailValue = emailInput.value;
+
+  if (!isValidEmail(emailValue)) {
+    event.preventDefault();
+    showError();
+  } else {
+    deleteErrorMessage();
+  }
+});
