871d3447ea8c8fbf0d189e94c05a84f53e7e5e32
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-06 18:45:53 -0300

    Fix js linters

--- Form.js
+++ Form.js
@@ -2,13 +2,11 @@
 // <--------------------------FORM-SECTION-VAR-------------------------->
 // *************************************************************************************************
 
-const form = document.querySelector("#form");
-const emailInput = document.querySelector("#email");
-const errorMsgContainer = document.querySelector("#errorMsg");
-const errorMsg =
-  "* Hi there! Please, write the content of the email field in lower case, thanks.*";
-const regexPattern =
-  /^[a-z0-9.!#$%&'+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)$/;
+const form = document.querySelector('#form');
+const emailInput = document.querySelector('#email');
+const errorMsgContainer = document.querySelector('#errorMsg');
+const errorMsg = '* Hi there! Please, write the content of the email field in lower case, thanks.*';
+const regexPattern = /^[a-z0-9.!#$%&'+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)$/;
 // *************************************************************************************************
 // <--------------------------FUNCTIONS-------------------------->
 // *************************************************************************************************
@@ -17,13 +15,13 @@ function isValidEmail(emailValue) {
 }
 
 function displayError() {
-  errorMsgContainer.classList.add("showError");
+  errorMsgContainer.classList.add('showError');
   errorMsgContainer.textContent = errorMsg;
 }
 
 function deleteErrorMsg() {
-  errorMsgContainer.classList.remove("showError");
-  errorMsgContainer.innerHTML = "";
+  errorMsgContainer.classList.remove('showError');
+  errorMsgContainer.innerHTML = '';
 }
 
 form.addEventListener('submit', (event) => {
