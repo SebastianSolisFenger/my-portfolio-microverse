2138bd6af8f09f6d4471cb65cb3069028ef8ba7a
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-07 14:15:32 -0300

    Add proto data local storage and fix linters

--- Form.js
+++ Form.js
@@ -3,7 +3,9 @@
 // *************************************************************************************************
 
 const form = document.querySelector('#form');
+const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#msg-text');
 const errorMsgContainer = document.querySelector('#errorMsg');
 const errorMsg = '* Hi there! Please, write the content of the email field in lower case, thanks.*';
 const regexPattern = /^[a-z0-9.!#$%&'+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)$/;
@@ -33,4 +35,26 @@ form.addEventListener('submit', (event) => {
   } else {
     deleteErrorMsg();
   }
+});
+
+// *************************************************************************************************
+// <--------------------------LOCAL-STORAGE-------------------------->
+// *************************************************************************************************
+
+const saveDataLocal = () => {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    msg: messageInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+};
+nameInput.addEventListener('change', saveDataLocal);
+emailInput.addEventListener('change', saveDataLocal);
+messageInput.addEventListener('change', saveDataLocal);
+window.addEventListener('load', () => {
+  const formData = JSON.parse(localStorage.getItem('formData'));
+  nameInput.value = formData.name;
+  emailInput.value = formData.email;
+  messageInput.value = formData.msg;
 });
